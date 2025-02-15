d8c39a4b8196ba5dbba060de017185e0f9bf6521
hammtah <115116715+hammtah@users.noreply.github.com>
2025-02-15 18:36:06 +0100

    group components in one folder & add db.php for db connection

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$dbname = "foot";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
